5871ca2d8a49f919419a5a1cf65f41716f3ca167
TheCop <kap-kirill@yandex.ru>
2015-03-19 12:51:20 +0300

    +pom executable

--- src/com/thecop/rpgtest/Main.java
+++ src/com/thecop/rpgtest/Main.java
@@ -1,8 +1,28 @@
 package com.thecop.rpgtest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Msven test!");
+        System.out.print("Enter something:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String s = br.readLine();
+            System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
