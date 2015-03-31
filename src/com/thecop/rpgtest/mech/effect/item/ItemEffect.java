f34af07b8afd8c617b0a82e3a505f9a7300a696e
TheCop <kap-kirill@yandex.ru>
2015-03-31 16:07:38 +0300

    +item effect abstract class

---
+++ src/com/thecop/rpgtest/mech/effect/item/ItemEffect.java
@@ -0,0 +1,24 @@
+package com.thecop.rpgtest.mech.effect.item;
+
+import com.thecop.rpgtest.mech.effect.Effect;
+
+/**
+ * Created by Admin on 31.03.2015.
+ */
+public abstract class ItemEffect extends Effect {
+    protected String description;
+
+    public ItemEffect(String name, String description) {
+        super(name);
+        this.description = description;
+    }
+
+    public ItemEffect(ItemEffect other) {
+        super(other);
+        this.description = other.description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
